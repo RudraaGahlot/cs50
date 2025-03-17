429832c3e37fa4e8eb72a78b257e3a87e4624382
RudraaGahlot <rudragahlot111@gmail.com>
2025-03-17 14:37:47 +0530

    made reusable functions & learn about do while loop

--- 01_basics/loop.c
+++ 01_basics/loop.c
@@ -1,16 +1,53 @@
 #include <stdio.h>
+#include <cs50.h>
+// int main(){
+//     // while loop
+//     int i = 0;
+//     while (i < 10) {
+//         printf("meow\n");
+//         i++;
+//     }
 
-int main(){
-    // while loop
-    int i = 0;
-    while (i < 10) {
-        printf("meow\n");
-        i++;
+//     // for loop
+//     for (int i = 0; i < 10; i++){
+//         printf("meow\n");
+//     }
+
+// }
+
+// void meow(int n);
+
+
+// int main () {
+//     meow(4);
+// }
+
+// void meow(int n) {
+//     for (int i = 0; i < n; i++){
+//         printf("meow\n");
+//     }
+// }
+
+int get_positive_integer(void);
+void meow(int n);
+
+int main (){
+    int times = get_positive_integer();
+    meow(times);
+}
+
+int get_positive_integer(void) {
+    int n;
+    do 
+    {
+        n = get_int("Number: ");
     }
+    while(n < 1);
+    return n;
+}
 
-    // for loop
-    for (int i = 0; i < 10; i++){
+void meow(int n){
+    for (int i = 0; i < n; i++){
         printf("meow\n");
     }
-
 }
