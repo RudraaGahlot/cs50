09473ad69240cf9ca9acdfb63e307b51b0bb9f0e
RudraaGahlot <rudragahlot111@gmail.com>
2025-03-17 13:00:26 +0530

    loop

---
+++ 01_basics/loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    // while loop
+    int i = 0;
+    while (i < 10) {
+        printf("meow\n");
+        i++;
+    }
+
+    // for loop
+    for (int i = 0; i < 10; i++){
+        printf("meow\n");
+    }
+
+}
